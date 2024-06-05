b1360fdf10b5b3169b97462acc637cd0e97418e4
Alan Saji <rockstaralansaji@gmail.com>
2024-06-05 14:45:28 +0530

    routing complete

---
+++ RouterProject/src/components/Github/Github.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import {useLoaderData} from 'react-router-dom'
+
+function Github() {
+    const data = useLoaderData()
+    // const [data, setData] = useState([]) 
+
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/Alansaji2003')
+    //    .then(response => response.json())
+    //    .then(data => {
+    //     console.log(data);
+    //     setData(data)})
+        
+        
+    // }, [])
+    
+    
+  return (
+    <>
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}
+    <img src={data.avatar_url} alt="Avatar" className="rounded-full h-64 w-64 mx-auto" />
+    </div>
+    
+    </>
+    
+  )
+}
+
+export default Github
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/Alansaji2003')
+    return response.json()
+}
